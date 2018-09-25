277e163ee0e5929a9a377d20261ef7cd32389637
HuTongsama <hutongsama@163.com>
2018-09-25 19:03:48 +0800

    Update Sales_item.h

--- Practice/ch07/Sales_item.h
+++ Practice/ch07/Sales_item.h
@@ -8,11 +8,15 @@ struct Sales_data
 	Sales_data(const std::string &s) :bookNo(s) {}
 	Sales_data(const std::string &s, unsigned n, double p) :bookNo(s), units_solds(n), revenue(n*p) {}
 	Sales_data(std::istream &is);
-	
 	std::string isbn()const { return bookNo; }
 	Sales_data& combine(const Sales_data&);
 	double avg_price()const;
 
+	friend Sales_data add(const Sales_data&, const Sales_data&);
+	friend std::ostream &print(std::ostream&, const Sales_data&);
+	friend std::istream &read(std::istream&, Sales_data&);
+
+private:
 	std::string bookNo;
 	unsigned units_solds = 0;
 	double revenue = 0.0;
